6173bc170288c58bb0e8af4c0cb3ca7d5035def1
Sabrina Rodrigues Fernandes <sabrinarfernandes.srf@gmail.com>
2022-10-16 12:26:56 -0300

    Classe LocadoraVeiculos.java para teste com a main

---
+++ LocadoraVeiculos.java
@@ -0,0 +1,17 @@
+public class LocadoraVeiculos {
+	public static void main(String[] args) {
+        Veiculo lista = new Veiculo();
+        Veiculo v1 = new Veiculo("ABC1234");
+        Veiculo v2 = new Veiculo("CDA4444");
+        lista.add(v1);
+        lista.add(v2);
+        v1.setPlaca("OOO9999");
+        v2.setAno(2022);
+        v2.setValorD(50.00);
+
+        
+        
+        System.out.println(lista.toString());
+	}
+}
+
