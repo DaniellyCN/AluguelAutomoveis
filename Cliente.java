eb29a41d6390885f133ff08da9afcd11d0db6692
JonesGamer18 <106637017+JonesGamer18@users.noreply.github.com>
2022-11-05 15:07:15 -0300

    Atualização dos atributos telefone,cep,cpf

--- Cliente.java
+++ Cliente.java
@@ -1,16 +1,17 @@
 
-public class Cliente{
+public class Cliente {
     private String nome;
-    private int cpf;
+    private long cpf;
     private int cnh;
     private String rua;
     private int numCasa;
     private String bairro;
     private String cidade;
-    private int cep;
-    private int telefone;
+    private long cep;
+    private long telefone;
 
-    public Cliente(String nome, int cpf, int cnh, String rua, int numCasa, String bairro, String cidade, int cep, int telefone){
+    public Cliente(String nome, long cpf, int cnh, String rua, int numCasa, String bairro, String cidade, long cep,
+            long telefone) {
         this.nome = nome;
         this.cpf = cpf;
         this.cnh = cnh;
@@ -22,28 +23,82 @@ public class Cliente{
         this.telefone = telefone;
     }
 
-    public String getNome(){return nome;}
-    public int getCpf(){return cpf;}
-    public int getCnh(){return cnh;}
-    public String getRua(){return rua;}
-    public int getNumeroCasa(){return numCasa;}
-    public String getBairro(){return bairro;}
-    public String getCidade(){return cidade;}
-    public int cep(){return cep;}
-    public int getTelefone(){return telefone;}
+    public String getNome() {
+        return nome;
+    }
 
-    public void setNome(String nome){this.nome = nome;}
-    public void setCpf(int cpf){this.cpf = cpf;}
-    public void setCnh(int cnh){this.cnh = cnh;}
-    public void setRua(String rua){this.rua = rua;}
-    public void setNumeroCasa(int numero){numCasa = numero;}
-    public void setBairro(String bairro){this.bairro = bairro;}
-    public void setCidade(String cidade){this.cidade = cidade;}
-    public void setCep(int cep){this.cep = cep;}
-    public void setTelefone(int telefone){this.telefone = telefone;}
-    
-    public String toString (){
-        return "Nome: "+nome+"CPF"+cpf+"cnh"+ cnh+"rua"+rua+"numero da casa"+numCasa+"bairro"+bairro+"cidade"+cidade+"cep"+cep+"telefone"+telefone;
+    public long getCpf() {
+        return cpf;
+    }
+
+    public int getCnh() {
+        return cnh;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public int getNumeroCasa() {
+        return numCasa;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public long cep() {
+        return cep;
+    }
+
+    public long getTelefone() {
+        return telefone;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCpf(long cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setCnh(int cnh) {
+        this.cnh = cnh;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public void setNumeroCasa(int numero) {
+        numCasa = numero;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public void setCep(long cep) {
+        this.cep = cep;
+    }
+
+    public void setTelefone(long telefone) {
+        this.telefone = telefone;
+    }
+
+    public String toString() {
+        return "Nome: " + nome + " CPF: " + cpf + " cnh: " + cnh + " rua: " + rua + " numero da casa: " + numCasa
+                + " bairro: "
+                + bairro + " cidade: " + cidade + " cep: " + cep + " telefone: " + telefone;
     }
 
 }
