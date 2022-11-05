1c8cca25433def84a02c2769e3094de6e1f949af
JonesGamer18 <106637017+JonesGamer18@users.noreply.github.com>
2022-11-05 15:14:36 -0300

    Opção verificar conta funcionando

--- GerenciarClienteConsole.java
+++ GerenciarClienteConsole.java
@@ -1,21 +1,23 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
-public class GerenciarClienteConsole{
+public class GerenciarClienteConsole {
     private GerenciarClientes gerenciarClientes;
     Scanner entrada = new Scanner(System.in);
-    
+    static ArrayList<Cliente> listaDClientes = new ArrayList<>();
 
-    public GerenciarClienteConsole (GerenciarClientes gerenciarClientes){
+    public GerenciarClienteConsole(GerenciarClientes gerenciarClientes) {
         this.gerenciarClientes = gerenciarClientes;
     }
 
-    public GerenciarClienteConsole(){
+    public GerenciarClienteConsole() {
 
     }
 
-    public void menuCadastroCliente(){
-        int op; 
-        do{
+    public void menuCadastroCliente() {
+        int op;
+        do {
             System.out.println("-------- Cadastro de clientes -------");
             System.out.println("1.Abrir conta");
             System.out.println("2.Verificar conta");
@@ -24,82 +26,85 @@ public class GerenciarClienteConsole{
 
             op = entrada.nextInt();
             entrada.nextLine();
-            switch(op){
-                case 1: 
-                abrirConta();
+            switch (op) {
+                case 1:
+                    abrirConta();
                     break;
-                case 2: 
-                verificarConta();
+                case 2:
+                    verificarConta();
                     break;
                 case 3:
-                deletarConta();
-                    default: 
+                    deletarConta();
+                default:
                 case 4:
-                sair();
-                    //System.out.println("Opção inválida");
+                    sair();
+                    // System.out.println("Opção inválida");
             }
-        }while(op!=0); 
+        } while (op != 0);
     }
 
+    public void abrirConta() {
 
-    public void abrirConta(){
-        
         Cliente cliente = new Cliente(null, 0, 0, null, 0, null, null, 0, 0);
-            System.out.println("----------------------------------------------------------------------------");
-            System.out.println("Abertura de conta");
-            System.out.print("Informe o seu nome : ");
-            cliente.setNome(entrada.nextLine());
+        System.out.println("----------------------------------------------------------------------------");
+        System.out.println("Abertura de conta");
+        System.out.print("Informe o seu nome : ");
+        cliente.setNome(entrada.nextLine());
 
-            System.out.print("Informe o CPF do titular: ");
-            cliente.setCpf(entrada.nextInt());
-            entrada.nextLine();
-            System.out.print("Informe a rua: ");
-            cliente.setRua(entrada.nextLine());
+        System.out.print("Informe o CPF do titular: ");
+        cliente.setCpf(entrada.nextLong());
+        entrada.nextLine();
+        System.out.print("Informe a rua: ");
+        cliente.setRua(entrada.nextLine());
 
-            System.out.print("Informe o numCasa: ");
-            cliente.setNumeroCasa(entrada.nextInt());
+        System.out.print("Informe o numCasa: ");
+        cliente.setNumeroCasa(entrada.nextInt());
 
-            System.out.print("Informe o bairro: ");
-            cliente.setBairro(entrada.nextLine());
-            entrada.nextLine();
-            System.out.print("Informe o cidade: ");
-            cliente.setCidade(entrada.nextLine());
+        System.out.print("Informe o bairro: ");
+        cliente.setBairro(entrada.nextLine());
+        entrada.nextLine();
+        System.out.print("Informe o cidade: ");
+        cliente.setCidade(entrada.nextLine());
 
-            System.out.print("Informe o telefone do titular: ");
-            cliente.setTelefone(entrada.nextInt());
+        System.out.print("Informe o telefone do titular: ");
+        cliente.setTelefone(entrada.nextLong());
 
-            System.out.println("Conta cadastrada!!");
-            System.out.println("----------------------------------------------------------------------------");
-            add(cliente);
-        
+        System.out.println("Conta cadastrada!!");
+        System.out.println("----------------------------------------------------------------------------");
+        GerenciarClienteConsole.add(cliente);
+
     }
 
-    private void add(Cliente cliente) {
-       gerenciarClientes.add(cliente);
+    private static void add(Cliente cliente) {
+        listaDClientes.add(cliente);
     }
-    public void verificarConta(){
-        System.out.println(gerenciarClientes.getResumoInfo());
+
+    public void verificarConta() {
+        System.out.println(listaDClientes.toString());
     }
-    public void deletarConta(){
-       
-        int opcao; 
-        
+
+    public void deletarConta() {
+
+        int opcao;
+
         System.out.println("\n\nDeseja excluir esse cadastro ?\n  1.Sim\n  2.Não");
         opcao = entrada.nextInt();
-        if(opcao == 1){
+        if (opcao == 1) {
             System.out.println("Digite seu CPF:");
             long cpf = entrada.nextLong();
-            
-            if(gerenciarClientes.existe(cpf)){
+
+            if (gerenciarClientes.existe(cpf)) {
                 gerenciarClientes.remove(cpf);
                 System.out.println("Conta excluida!!");
             }
-            }if(opcao == 2){
-                System.out.println("processo cancelado");
-            }
-   
+        }
+        if (opcao == 2) {
+            System.out.println("processo cancelado");
+        }
+
     }
-    public void sair (){
+
+    public void sair() {
         MenuPrincipal menuPrincipal = new MenuPrincipal();
         menuPrincipal.menuPrincipal();
     }
